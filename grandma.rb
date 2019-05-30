ce9e8e98c3177380deaa34b3179e1ce00449d1f3
Ali Karimi <ajkarimi7@gmail.com>
2019-05-30 21:49:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,5 +23,7 @@ def speak_to_grandma(phrase)
   if phrase == 'Hi Nana, how are you?'
     return 'HUH?! SPEAK UP, SONNY!'
   end
-  
+  if phrase == 'WHAT DID YOU EAT TODAY?'
+    return 'NO, NOT SINCE 1938!'
+  end 
 end
